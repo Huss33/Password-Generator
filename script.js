9d1f1fffc9bf3ff0949e226dfcacfdf84407323c
Bear Ridge <ahuss2010@live.com>
2021-12-17 20:38:14 -0600

    initial commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-var lowercaseChar = ["a","b","c","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var lowercaseChar = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var uppercaseChar = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var numbericChar = ["0","1","2","3","4","5","6","7","8","9"];
 var specialChar = ["!","#","$","%","&","(",")","*","+","-",".","/",":","<","=",">","?","@","[","^"];
@@ -23,16 +23,49 @@ function writePassword() {
 
 function generatePassword() {
   var lengthInput = prompt("Length of Password?");
+  
     if (lengthInput >= 8 && lengthInput <= 128) {
+      var concatArray = []
       // var pLength = document.createElement(lengthInput);
-      } else {
-        alert("Please enter a number between 8 and 128");
+      var lowerInput = confirm("Would you like to inclued lowercase? Select OK (for yes) or Cancel (for no)");   
+      var upperInput = confirm("Would you like to inclued uppercase?");
+      var numberInput = confirm("Would you like to inclued numbers?");
+      var specialInput = confirm("Would you like to inclued special characters?");
+      // console.log(lowerInput,upperInput,numberInput,specialInput);
+        if (lowerInput) {
+         concatArray = concatArray.concat(lowercaseChar)
+        }
+        if (upperInput) {
+          concatArray = concatArray.concat(uppercaseChar)
+         }
+        if (numberInput) {
+          concatArray = concatArray.concat(numbericChar)
+         }
+        if (specialInput) {
+          concatArray = concatArray.concat(specialChar)
+        }
+        if (!lowerInput && !upperInput && !numberInput && !specialInput) {
+          concatArray = lowercaseChar.concat(uppercaseChar, numbericChar, specialChar)   
+        } 
+         
+         // console.log(concatArray);
+        var charArrayChoice = "";
+
+        for (let i = 1; i <= lengthInput; i++) {
+          
+          var randomChar = random(concatArray);
+          charArrayChoice = charArrayChoice + randomChar
+        }
+        // console.log(charArrayChoice)
+        return charArrayChoice;
+    } else {
+        // (!lowerInput && !upperInput && !numberInput && !specialInput)
+          // alert("Must select at least one character genre!")
+          alert("Please enter a number between 8 and 128");
         generatePassword()
-      }
-    lowerInput = confirm("Would you like to inclued lowercase? Select OK (for yes) or Cancel (for no)");   
-    upperInput = confirm("Would you like to inclued uppercase?");
-    numberInput = confirm("Would you like to inclued numbers?");
-    specialInput = confirm("Would you like to inclued special characters?");
+      } 
+
+   
 }
   //return the generated password
 
@@ -45,7 +78,8 @@ generateBtn.addEventListener("click", writePassword);
 
 
 function randomInt(limit) {
-    return Math.floor(Math.random() * Math.floor(limit))
+  console.log(limit);
+    return Math.floor(Math.random() * limit)
 }
 
 function random(selection) {
@@ -56,14 +90,14 @@ function random(selection) {
 
   // var userInput = (lowerInput + upperInput + numberInput + specialInput);
 
-  var charArrayChoice = "";
+  // var charArrayChoice = "";
   
-  var totalCharArray = lowercaseChar.concat(uppercaseChar,numbericChar,specialChar);
+  // var totalCharArray = lowercaseChar.concat(uppercaseChar,numbericChar,specialChar);
 
-  for (var i = 0; i < 128; i++) {
-      charArrayChoice += random(totalCharArray);
-  }
-console.log(charArrayChoice);
+//   for (var i = 0; i < 128; i++) {
+//       charArrayChoice += random(totalCharArray);
+//   }
+// console.log(charArrayChoice);
 //   function random_item(items)
 // {
 //     return items[Math.floor(Math.random()*items.length)];
